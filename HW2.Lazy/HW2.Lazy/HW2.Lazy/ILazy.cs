0d14daed9d49dd4b72dc567238486d1c741ab99e
bogdanovaarina <arinabogdanova1967@mail.ru>
2025-09-23 21:38:49 +0300

    add solution

---
+++ HW2.Lazy/HW2.Lazy/HW2.Lazy/ILazy.cs
@@ -0,0 +1,65 @@
+// <copyright file="ILazy.cs" author="bogdanovaarina">
+// under MIT License
+// </copyright>
+
+namespace DefaultNamespace;
+
+public interface ILazy<T>
+{
+    T Get();
+}
+
+public class SimpleLazy<T> : ILazy<T>
+{
+    private Func<T> supplier;
+    private T value;
+    private bool isValueCreated;
+
+    public SimpleLazy(Func<T> supplier)
+    {
+        this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+        this.isValueCreated = false;
+    }
+
+    public T Get()
+    {
+        if (!isValueCreated)
+        {
+            value = supplier();
+            supplier = null;
+            isValueCreated = true;
+        }
+        return value;
+    }
+}
+
+public class TrreadSafeLazy<T> : ILazy<T>
+{
+    private Func<T> supplier;
+    private T value;
+    private bool isValueCreated;
+    private readonly var lockObject = new object();
+    
+    public ThreadSafeLazy(Func<T> supplier)
+    {
+        this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+        this.isValueCreated = false;
+    }
+
+    public T Get()
+    {
+        if (!isValueCreated)
+        {
+            lock(lockObject)
+            {
+                if (!isValueCreated)
+                {
+                    value = supplier();
+                    supplier = null;
+                    isValueCreated = true;
+                }
+            }
+        }
+        return value;
+    }
+}
---
+++ HW2.Lazy/HW2.Lazy/Tests/Tests/Tests.cs
@@ -0,0 +1,89 @@
+﻿// <copyright file="NotSimple.cs" author="bogdanovaarina">
+// under MIT License
+// </copyright>
+
+using HW2.Lazy;
+
+namespace Tests;
+
+public class Tests
+{
+    [Test]
+    public void SimpleLazy_Get_CallsSupplierOnlyOnce()
+    {
+        int callCount = 0;
+        var lazy = new SimpleLazy<int>(() => ++callCount);
+
+        var result1 = lazy.Get();
+        var result2 = lazy.Get();
+        var result3 = lazy.Get();
+
+        Assert.AreEqual(1, result1);
+        Assert.AreEqual(1, result2);
+        Assert.AreEqual(1, result3);
+        Assert.AreEqual(1, callCount);
+    }
+    
+    [Test]
+    public void SimpleLazy_Get_WithNullValue()
+    {
+        var lazy = new SimpleLazy<object>(() => null);
+
+        Assert.IsNull(lazy.Get());
+        Assert.IsNull(lazy.Get());
+    }
+    
+    [Test]
+    public void SimpleLazy_Constructor_ThrowsOnNullSupplier()
+    {
+        Assert.Throws<ArgumentNullException>(() => new SimpleLazy<object>(null));
+    }
+    
+    [Test]
+    public void ThreadSafeLazy_Get_CallsSupplierOnlyOnce()
+    {
+        int callCount = 0;
+        var lazy = new ThreadSafeLazy<int>(() => ++callCount);
+
+        var result1 = lazy.Get();
+        var result2 = lazy.Get();
+        var result3 = lazy.Get();
+
+        Assert.AreEqual(1, result1);
+        Assert.AreEqual(1, result2);
+        Assert.AreEqual(1, result3);
+        Assert.AreEqual(1, callCount);
+    }
+    
+    public void ThreadSafeLazy_Multithreaded_NoRaceConditions()
+    {
+        int callCount = 0;
+        var lazy = new ThreadSafeLazy<int>(() =>
+        {
+            Thread.Sleep(100);
+            return Interlocked.Increment(ref callCount);
+        });
+
+        const int threadCount = 10;
+        var results = new int[threadCount];
+        var threads = new Thread[threadCount];
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            var index = i;
+            threads[i] = new Thread(() => results[index] = lazy.Get());
+            threads[i].Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        Assert.AreEqual(1, callCount);
+        foreach (var result in results)
+        {
+            Assert.AreEqual(1, result);
+        }
+    }
+}
