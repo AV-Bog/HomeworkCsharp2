2e89ca040400fac72ea6a1d869362ae1c884fb1e
bogdanovaarina <arinabogdanova1967@mail.ru>
2025-09-28 10:39:01 +0300

    like everything else

--- HW2.Lazy/HW2.Lazy/HW2.Lazy/ILazy.cs
+++ HW2.Lazy/HW2.Lazy/HW2.Lazy/ILazy.cs
@@ -1,65 +1,67 @@
 // <copyright file="ILazy.cs" author="bogdanovaarina">
 // under MIT License
 // </copyright>
+using System;
 
-namespace DefaultNamespace;
-
-public interface ILazy<T>
+namespace HW2.Lazy
 {
-    T Get();
-}
-
-public class SimpleLazy<T> : ILazy<T>
-{
-    private Func<T>? supplier;
-    private T? value;
-    private bool isValueCreated;
-
-    public SimpleLazy(Func<T> supplier)
+    public interface ILazy<T>
     {
-        this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
-        this.isValueCreated = false;
+        T Get();
     }
 
-    public T Get()
+    public class SimpleLazy<T> : ILazy<T>
     {
-        if (!isValueCreated)
+        private Func<T>? supplier;
+        private T? value;
+        private bool isValueCreated;
+
+        public SimpleLazy(Func<T> supplier)
         {
-            value = supplier!();
-            supplier = null;
-            isValueCreated = true;
+            this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+            this.isValueCreated = false;
         }
-        return value;
-    }
-}
 
-public class ThreadSafeLazy<T> : ILazy<T>
-{
-    private Func<T>? supplier;
-    private T? value;
-    private bool isValueCreated;
-    private readonly object lockObject = new object();
-    
-    public ThreadSafeLazy(Func<T> supplier)
-    {
-        this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
-        this.isValueCreated = false;
-    }
-
-    public T Get()
-    {
-        if (!isValueCreated)
+        public T Get()
         {
-            lock(lockObject)
+            if (!isValueCreated)
             {
-                if (!isValueCreated)
+                value = supplier!();
+                supplier = null;
+                isValueCreated = true;
+            }
+            return value!;
+        }
+    }
+
+    public class ThreadSafeLazy<T> : ILazy<T>
+    {
+        private Func<T>? supplier;
+        private T? value;
+        private bool isValueCreated;
+        private readonly object lockObject = new object();
+
+        public ThreadSafeLazy(Func<T> supplier)
+        {
+            this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+            this.isValueCreated = false;
+        }
+
+        public T Get()
+        {
+            if (!isValueCreated)
+            {
+                lock (lockObject)
                 {
-                    value = supplier!();
-                    supplier = null;
-                    isValueCreated = true;
+                    if (!isValueCreated)
+                    {
+                        value = supplier!();
+                        supplier = null;
+                        isValueCreated = true;
+                    }
                 }
             }
+            return value!;
         }
-        return value;
     }
 }
