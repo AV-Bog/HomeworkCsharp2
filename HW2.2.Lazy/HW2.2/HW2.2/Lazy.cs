343bcd22b7dc6fef10eb3bd9fc62f24c48bfcb81
bogdanovaarina <arinabogdanova1967@mail.ru>
2025-10-01 00:34:25 +0300

    add solution

---
+++ HW2.2.Lazy/HW2.2/HW2.2/Lazy.cs
@@ -0,0 +1,67 @@
+﻿// <copyright file="ILazy.cs" author="bogdanovaarina">
+// under MIT License
+// </copyright>
+using System;
+
+namespace HW2.Lazy
+{
+    public interface ILazy<T>
+    {
+        T Get();
+    }
+
+    public class SimpleLazy<T> : ILazy<T>
+    {
+        private Func<T>? supplier;
+        private T? value;
+        private bool isValueCreated;
+
+        public SimpleLazy(Func<T> supplier)
+        {
+            this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+            this.isValueCreated = false;
+        }
+
+        public T Get()
+        {
+            if (!isValueCreated)
+            {
+                value = supplier!();
+                supplier = null;
+                isValueCreated = true;
+            }
+            return value!;
+        }
+    }
+
+    public class ThreadSafeLazy<T> : ILazy<T>
+    {
+        private Func<T>? supplier;
+        private T? value;
+        private bool isValueCreated;
+        private readonly object lockObject = new object();
+
+        public ThreadSafeLazy(Func<T> supplier)
+        {
+            this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+            this.isValueCreated = false;
+        }
+
+        public T Get()
+        {
+            if (!isValueCreated)
+            {
+                lock (lockObject)
+                {
+                    if (!isValueCreated)
+                    {
+                        value = supplier!();
+                        supplier = null;
+                        isValueCreated = true;
+                    }
+                }
+            }
+            return value!;
+        }
+    }
+}
---
+++ HW2.2.Lazy/HW2.2/Tests/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿// <copyright file="NotSimple.cs" author="bogdanovaarina">
+// under MIT License
+// </copyright>
+
+using System;
+using System.Threading;
+using HW2.Lazy;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SimpleLazy_Get_CallsSupplierOnlyOnce()
+        {
+            int callCount = 0;
+            var lazy = new SimpleLazy<int>(() => ++callCount);
+
+            var result1 = lazy.Get();
+            var result2 = lazy.Get();
+            var result3 = lazy.Get();
+
+            Assert.That(result1, Is.EqualTo(1));
+            Assert.That(result2, Is.EqualTo(1));
+            Assert.That(result3, Is.EqualTo(1));
+            Assert.That(callCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SimpleLazy_Constructor_ThrowsOnNullSupplier()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SimpleLazy<object>(null!));
+        }
+
+        [Test]
+        public void ThreadSafeLazy_Get_CallsSupplierOnlyOnce()
+        {
+            int callCount = 0;
+            var lazy = new ThreadSafeLazy<int>(() => ++callCount);
+
+            var result1 = lazy.Get();
+            var result2 = lazy.Get();
+            var result3 = lazy.Get();
+
+            Assert.That(result1, Is.EqualTo(1));
+            Assert.That(result2, Is.EqualTo(1));
+            Assert.That(result3, Is.EqualTo(1));
+            Assert.That(callCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ThreadSafeLazy_Multithreaded_NoRaceConditions()
+        {
+            int callCount = 0;
+            var lazy = new ThreadSafeLazy<int>(() =>
+            {
+                Thread.Sleep(100);
+                return Interlocked.Increment(ref callCount);
+            });
+
+            const int threadCount = 10;
+            var results = new int[threadCount];
+            var threads = new Thread[threadCount];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                var index = i;
+                threads[i] = new Thread(() => results[index] = lazy.Get());
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Assert.That(callCount, Is.EqualTo(1));
+            foreach (var result in results)
+            {
+                Assert.That(result, Is.EqualTo(1));
+            }
+        }
+    }
