5a7f753da06443d9f384f7837c09a0314752531e
bogdanovaarina <arinabogdanova1967@mail.ru>
2025-12-01 23:47:39 +0300

    добавляю проектик для демонстрации темы доклада на рил коде. да просят меня за рус коммы

---
+++ report/MauiCalc/MauiCalc/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiCalc;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        ResultLabel.Text = "Результат: Работает!";
+    }
+}
